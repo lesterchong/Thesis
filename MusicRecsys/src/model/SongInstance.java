a539376c7b41e3199b765bcfbc2a630d8c3779f6
mikeyboy213 <mikeyboy213@192.168.1.6>
2014-11-23 12:15:19 +0800

    done without testing

--- MusicRecsys/src/model/SongInstance.java
+++ MusicRecsys/src/model/SongInstance.java
@@ -5,14 +5,18 @@
  */
 package model;
 
+import java.util.LinkedList;
+
 /**
  *
  * @author mikeyboy213
  */
 public class SongInstance {
     private int id;
+    private LinkedList <Float> attributes;
     private String cluster;
     private int x, y;
+    private float tempdistance;
 
     /**
      * @return the id
@@ -68,6 +72,34 @@ public class SongInstance {
      */
     public void setY(int y) {
         this.y = y;
+    }
+
+    /**
+     * @return the attributes
+     */
+    public LinkedList <Float> getAttributes() {
+        return attributes;
+    }
+
+    /**
+     * @param attributes the attributes to set
+     */
+    public void setAttributes(LinkedList <Float> attributes) {
+        this.attributes = attributes;
+    }
+
+    /**
+     * @return the tempdistance
+     */
+    public float getTempdistance() {
+        return tempdistance;
+    }
+
+    /**
+     * @param tempdistance the tempdistance to set
+     */
+    public void setTempdistance(float tempdistance) {
+        this.tempdistance = tempdistance;
     }
     
 }
